00760911579701b15bee9032291db738ef00864c
Mateusz <mateuszskc@gmail.com>
2024-03-17 22:18:59 +0100

    variables (PL.zmienne) const and let

--- js/style.js
+++ js/style.js
@@ -1,2 +1,20 @@
-console.log('Cześć');
-console.log('Za pomocą console.log możemy wyświetlać nasze zmienne w konsoli');
+const name = 'Mateusz';
+
+console.log(name);
+
+let age = 29
+console.log(age);
+
+age = 'jakiś tekst, niekoniecznie wiek'
+console.log(age);
+
+let color;
+
+color = 'red'
+console.log(color);
+
+let myName = 'Mateusz'
+let myAge = 30
+let favoriteFood = 'kebab'
+
+console.log(myName, myAge, favoriteFood);
