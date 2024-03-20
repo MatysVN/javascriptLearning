66a66e4fa81f5e708ca1a1a536e036527cedbfeb
Mateusz <mateuszskc@gmail.com>
2024-03-20 19:04:15 +0100

    boolean

--- js/style.js
+++ js/style.js
@@ -1,20 +1,18 @@
-const num1 = 23;
-const num2 = "45";
+/* 
+! TO ZAWSZE ZWRÓCI FALSE !
 
-//łącząc typ number z typem string otrzymujemy typ string
-console.log(num1 + num2);
+false
+undefined
+null
+0
+Nan
+'' - pusty string
 
-const num3 = "abcd";
+*/
 
-//mnożąc lub dzieląc typ string otrzymujemy typ number ( w tym przypadku dostaniemy odpowiedź NaN - Not a Number) *ale jest to tylko jako ciekawostka, używamy parseInt
-console.log(num3 * 1);
-
-const num4 = 1354.8356;
-//za pomocą metody .toFixed zaokrąglamy liczbę
-console.log(num4.toFixed());
-//jeżeli w nawias wpiszemy liczbę to do tylu miejsc po przecinku zaokrąli nam liczbę
-console.log(num4.toFixed(2));
-
-const num5 = "123 mateusz";
-//za pomocą .parseInt możemy pobrać typ string jako number, ale musi być tam wpisana liczba
-console.log(parseInt(num5));
+//Każda liczba różna od 0 czy string zwróci TRUE
+if (1) {
+    console.log('TRUE');
+} else {
+    console.log('FALSE');
+}
