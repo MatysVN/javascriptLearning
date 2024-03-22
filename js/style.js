95d1a626d40e428952440f26465a61a74fc06154
Mateusz <mateuszskc@gmail.com>
2024-03-22 12:32:04 +0100

    conditional instructions - SWITCH

--- js/style.js
+++ js/style.js
@@ -1,9 +1,39 @@
-const password = '213ads21aa!'
+const day = "środa";
 
-if(password.length > 10 && password.includes('!')) {
-	console.log(`Twoje hasło ma ${password.length} znaków. To jest rewelacyjne hasło!`);
-} else if(password.length > 5 && password.length <= 10) {
-	console.log(`Twoje hasło ma ${password.length} znaków. To jest średnie hasło.. Dodaj "!"`);
+//IF
+
+if (day === "poniedziałek") {
+	console.log("Dziś jest poniedziałek");
+} else if (day === "wtorek") {
+	console.log("Dziś jest wtorek");
+} else if (day === "środa") {
+	console.log("Dziś jest środa");
+} else if (day === "czwartek") {
+	console.log("Dziś jest czwartek");
+} else if (day === "piątek") {
+	console.log("Dziś jest piątek");
 } else {
-	console.log(`Twoje hasło ma tylko ${password.length} znaków. Musi być dłuższe...`);
+	console.log("Jest Weekend");
+}
+
+//SWITCH
+
+switch (day) {
+	case "poniedziałek":
+		console.log("Dziś jest poniedziałek");
+		break;
+	case "wtorek":
+		console.log("Dziś jest wtorek");
+		break;
+	case "środa":
+		console.log("Dziś jest środa");
+		break;
+	case "czwartek":
+		console.log("Dziś jest czwartek");
+		break;
+	case "piątek":
+		console.log("Dziś jest piątek");
+		break;
+	default:
+		console.log("WEEKEND");
 }
