18a64d049dff1f2951b611317da5198d652cb699
Mateusz <mateuszskc@gmail.com>
2024-03-22 14:58:41 +0100

    knowledge testing tasks

--- js/style.js
+++ js/style.js
@@ -1,24 +1,68 @@
-let x = 10
+console.log("Zadanie #1 - template string");
+const x = 50;
+const y = 30;
+console.log(`${x} jest większe niż ${y}`);
 
-if (x > 20) {
-	console.log(`${x} > 20`);
+console.log("-------");
+console.log("Zadanie #2 - if & else");
+const color = "blue";
+const newColor = "green";
+if (color == newColor) {
+	console.log("Kolory się zgadzają");
 } else {
-	console.log(`${x} < 20`);
+	console.log("Kolory się nie zgadzają");
 }
 
-//zamiast tego można napisać 
-const newX = x > 20 ? `${x} > jest większe niż 20` : `${x} < jest mniejsze niż 20`;
-console.log(newX);
-
-//-----------
-
-const isLoggedIn = true; 
-
-function loggedIn() {
-	console.log('Użytkownik jest zalogowany!');
-}
-function loggedOut() {
-	console.log('Użytkownik nie jest zalogowany...');
+console.log("-------");
+console.log("Zadanie #3 - if & else if & else");
+const a = 100;
+const b = 50;
+if (a > b) {
+	console.log(`a jest większe od b`);
+} else if (a === b) {
+	console.log(`a jest takie samo jak b`);
+} else {
+	console.log(`a jest mniejsze niż b`);
 }
 
-isLoggedIn ? loggedIn() : loggedOut();
+console.log("-------");
+console.log("Zadanie #4 - switch");
+const promo = "30%";
+switch (promo) {
+	case "10%":
+		console.log("Dziś mamy 10% zniżki!");
+		break;
+	case "20%":
+		console.log("Dziś mamy 20% zniżki!");
+		break;
+	case "30%":
+		console.log("Dziś mamy 30% zniżki!");
+		break;
+	default:
+		console.log("Śledź newsletter, aby mieć powiadomienia o zniżkach");
+		break;
+}
+
+console.log("-------");
+console.log("Zadanie #5 - Operator warunkowy");
+
+const m = 10;
+const check =
+	m % 2 === 0 ? `Liczba ${m} jest parzysta` : `liczba ${m} nie jest parzysta`;
+console.log(check);
+
+
+
+console.log("-------");
+console.log("Zadanie #6 - O");
+const o = 31;
+let text;
+
+if (o >= 100) {
+	text = `Zmienna "o" jest większa od 100`;
+} else if (o < 100 && o > 30) {
+	text = `Zmienna "o" jest średniakiem`;
+} else {
+	text = `Zmienna "o" jest mała`;
+}
+console.log(text.toUpperCase());
