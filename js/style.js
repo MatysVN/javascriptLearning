f7c6f85a905980cca4b959d75d003c4608b24f7e
Mateusz <mateuszskc@gmail.com>
2024-03-25 08:49:46 +0100

    for of loop

--- js/style.js
+++ js/style.js
@@ -1,32 +1,15 @@
-for (let i = 0; i < 6; i++) {
-	console.log(i);
+const numbers = [1, 2, 3, 4, 5];
+
+console.log('pętla for');
+//tak wygląda zwykła pętla for
+for (let i = 0; i < numbers.length; i++) {
+	console.log(numbers[i] * 2);
 }
 
-console.log("---------");
+console.log('-------');
+console.log('pętla for of');
 
-const animals = ['pies', 'kot', 'mysz', 'królik', 'lis', 'dzik']
-
-//zamiast pisać tak możemy użyć pętli
-// console.log(animals[0]);
-// console.log(animals[1]);
-// console.log(animals[2]);
-// console.log(animals[3]);
-// console.log(animals[4]);
-// console.log(animals[5]);
-
-//jednak ten sposób nie jest dobry ponieważ jeżeli zmieni się długośc naszej tablicy to elementy nie zostaną wylogowane
-// for (let i = 0; i < 6; i++) {
-// 	console.log(animals[i]);
-// }
-
-//zamiast tego możemy naspisać że pętla ma się wykonywać tyle razy ile jest elementów w tablicy
-for (let i = 0; i < animals.length; i++) {
-	console.log(animals[i]);
-}
-
-console.log('---------');
-
-const colors = ['red', 'green', 'blue', 'white', 'violet', 'orange', 'grey', 'yellow']
-for (let i = 0; i < colors.length; i++) {
-	console.log(colors[i]);
+//tak wygląda pętla for of
+for (const apple of numbers) {
+	console.log(apple * 2);
 }
