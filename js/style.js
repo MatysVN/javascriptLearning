1945795541f95631c8da0c2c6f7658c966917426
Mateusz <mateuszskc@gmail.com>
2024-03-27 10:52:00 +0100

    map vs foreach

--- js/style.js
+++ js/style.js
@@ -1,25 +1,11 @@
-const numbers = [0, 23, 48, 175, 2, 34, 11]
+const numbers = [0, 1, 2, 3]
 
-function number(x) {
-    return x % 2 === 0
-}
+//forEach jest to metoda (pętla) która wykonuje dany kod na danej tablicy, ale niczego nie zwraca dlatego mamy undefined (console.log jest tylko dla tego aby pokazać nam co robi funkcja)
+const newNumbers = numbers.forEach(num => console.log(num * 2))
+console.log(newNumbers);
 
-//filter(nazwafunkcji) wypisuje nam te elementy z tablicy które spełniają warunki funkcji, w tym przypadku konsola wypisze nam wszystkie liczby parzyste
-console.log('filter');
-console.log(numbers.filter(number));
+console.log('----');
 
-//na tablicy numbers dla każdego naszego elementu (num) wykonaj kod
-console.log('-----');
-console.log('forEach');
-numbers.forEach(num => console.log(num * 5))
-
-//sprawdzamy czy na tablicy numbers znajduje się typ number 0, jeśli byłby string '0' to pokaże false
-console.log('-----');
-console.log('include');
-console.log(numbers.includes(0));
-
-//sprawdzamy jaki index ma element w naszej tablicy, w tym przypadku przy wartości 175 znajdującej się w tablicy pokaże 3 ponieważ 175 ma index 3, przy 210 pokaże -1 ponieważ takiego elemntu nie ma w tablicy
-console.log('-----');
-console.log('indexOf');
-console.log(numbers.indexOf(175));
-console.log(numbers.indexOf(210));
+//map działa dokładnie jak forEach, ale zwraca nam nową tablicę (tutaj nie ma console.log w funkcji ponieważ zwróciło by 4x undefined)
+const mapNumbers = numbers.map(x => x * 2)
+console.log(mapNumbers);
