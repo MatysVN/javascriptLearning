ee9c3d5081d89e3f2e9edc61861e400820cf575c
Mateusz <mateuszskc@gmail.com>
2024-03-28 16:06:40 +0100

    function introduction

--- js/style.js
+++ js/style.js
@@ -1,66 +1,30 @@
-console.log("#1");
-const letters = ["c", "d"];
+//deklaracja funkcji - podlega hoistingowi, czyli wynosi funkcję na samą górę dlatego jeżeli najpiew wywołalibyśmy funkcję a następnie ją zapisali to będzie działać
 
-letters.unshift("a", "b");
-letters.push("e", "f");
-
-console.log(letters);
-console.log(letters.includes("c"));
-
-console.log(" ");
-console.log("#2");
-
-const numbers = [1, 2, 3];
-const food = ["🍔", "🍟", "🍕"];
-const menu = numbers.concat(food);
-console.log(menu);
-
-const spreadMenu = [...numbers, ...food];
-console.log(spreadMenu);
-
-console.log(" ");
-console.log("#3");
-
-const arr = [1, 5, 13, 26, 48];
-
-newArr = arr.map((x) => x * 5);
-console.log(newArr);
-
-for (x of newArr) {
-	if (x % 2 === 0) {
-		console.log(`Liczba Parzysta: ${x}`);
-	} else {
-		console.log(`Liczba Nieparzysta: ${x}`);
-	}
+function test() {
+	console.log('hello');
 }
 
-console.log(" ");
-console.log("#4");
+test()
 
-const colors = ["green"];
-colors.unshift("red");
-colors.push("blue");
+//aby przeciwdziałać hoistingowi możemy korzystać z wyrażeń funkcyjnych, ale najlepsze do tego są funkcje strzałkowe które niebawem poznam
 
-for (i = 0; i < colors.length; i++) {
-	console.log(`Mój ulubiony kolor to: ${colors[i].toUpperCase()}`);
+const helloWorld = function () {
+	console.log(`Hello World`);
 }
-console.log("======");
+helloWorld()
 
-for (i of colors) {
-	console.log(
-		`Mój ulubiony kolor to: ${i.charAt(0).toUpperCase() + i.slice(1)}`
-	);
+//funkcja z argumentami
+function add(parametr) {
+	console.log(`Podany argument to: ` + parametr);
 }
+add('Mateusz')
 
-console.log(" ");
-console.log("#5");
+function substract(num1, num2) {
+	console.log(num1 - num2);
+}
+substract(12, 4)
 
-const cars = "Audi, Mercedes, BMW, Nissan, Dodge";
-const carsArr = cars.split(", ");
-
-console.log(carsArr);
-const carsArrLength = carsArr.length > 3 ? `Jest OK` : `Nie jest OK`;
-console.log(carsArrLength);
-
-const carCheck = carsArr.indexOf('Audi') === -1 ? carsArr.pop() : carsArr.push('Kawasaki')
-console.log(carsArr);
+function user(a, b) {
+	console.log(`Cześć, mam na imię ${a} i mam ${b} lat`);
+}
+user('Mateusz', 30)
