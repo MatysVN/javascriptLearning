ffe6158706aca893c47430cace9d1fd1c2bba482
Mateusz <mateuszskc@gmail.com>
2024-03-28 22:59:10 +0100

    anonymous function

--- js/style.js
+++ js/style.js
@@ -1,30 +1,5 @@
-//deklaracja funkcji - podlega hoistingowi, czyli wynosi funkcję na samą górę dlatego jeżeli najpiew wywołalibyśmy funkcję a następnie ją zapisali to będzie działać
+const heading = document.querySelector('h1')
 
-function test() {
-	console.log('hello');
-}
-
-test()
-
-//aby przeciwdziałać hoistingowi możemy korzystać z wyrażeń funkcyjnych, ale najlepsze do tego są funkcje strzałkowe które niebawem poznam
-
-const helloWorld = function () {
-	console.log(`Hello World`);
-}
-helloWorld()
-
-//funkcja z argumentami
-function add(parametr) {
-	console.log(`Podany argument to: ` + parametr);
-}
-add('Mateusz')
-
-function substract(num1, num2) {
-	console.log(num1 - num2);
-}
-substract(12, 4)
-
-function user(a, b) {
-	console.log(`Cześć, mam na imię ${a} i mam ${b} lat`);
-}
-user('Mateusz', 30)
+heading.addEventListener('click', function() {
+	console.log('Kliknięto H1');
+})
