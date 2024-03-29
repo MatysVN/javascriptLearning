f5fbcabac215f94f5c72ff43e60d852ce06ff8c4
Mateusz <mateuszskc@gmail.com>
2024-03-29 23:34:40 +0100

    function ranges

--- js/style.js
+++ js/style.js
@@ -1,21 +1,36 @@
-console.log('=== SPREAD ===');
+//name jest w zakresie GLOBALNYM - czyli dostępna jest z każdego miejsca w naszym kodzie
+const name = 'Marysia'
 
-const arr = [1,2,3,4]
-console.log(arr);
-console.log(...arr);
+//ustalając zmienną food bez nadania jej wartości możemy używać jej w każdym zakresie nie musząc tworzyć za każdym razem nowej zmiennej. 
+let food
 
-console.log('');
-console.log('=== REST ===');
+//funcja test również jest w zakresie GLOBALNYM
+const test = () => {
+	//ten const name jest w zakresie LOKALNYM, dlatego możemy użyć nazwy name drugi raz
+	const name = 'Balbinka'
+	//ten console log jest w zakresie LOKALNYM
+	console.log(`NAME w funkcji - ${name}`);
+	//zmienna age jest w zakresie lokalnym dlatego nie możemy wyświetlić jej poza funkcją test
+	const age = 24
+	console.log(age);
 
-const numbers = (x, y, ...z) => {
-	console.log(x, y, z);
-	console.log('');
-	//skoro element z jest tablicą to możemy pracować z nim dokładnie tak samo jak z tablicami
-	console.log('-- każdy element z tablicy "z" * 2 --');
-	console.log(z.map(el => el * 2));
-console.log('');
-	//możemy również wyświetlać nie wszystkie elementy np tylko x, y
-	console.log('-- x + y --');
-	console.log(x + y);
+	const test2 = (name) => {
+		//konsola wyświetli najpiew name z tej funkcji jeżeli nie podamy żadnego name, to przejdzie poziom wyżej do funkcji test i wyświetli zmienną name Balbinka. Jeżeli nadal nie byłoby nadanej zmiennej to przejdzie poziom wyżej i wyświetli dane z zakresu GLOBALNEGO
+		console.log(name);
+	}
+	test2(name)
+
+	//funkcja test3 jest funkcją siostrzaną do test2, ale nie ma dostępu do jej wnętrza, tj nie możemy używać tego co przechowuje test2
+	const test3 = () => {
+		const name = "test3 - Matylda"
+		console.log(name);
+	}
+	test3()
 }
-numbers(11,12,13,14,15, 'string', true)
+test()
+console.log('');
+
+//ten console.log również jest w zakresie GLOBALNYM
+console.log(`NAME poza funkcją - ${name}`);
+
+
