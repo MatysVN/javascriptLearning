72a9c7e04fd13d3148af112173216c9e5e67dabf
Mateusz <mateuszskc@gmail.com>
2024-03-29 23:03:00 +0100

    Functions - Rest and Spread

--- js/style.js
+++ js/style.js
@@ -1,8 +1,21 @@
-const hello = (name = "drogi użytkowniku") => {
-	console.log(`Cześć ${name}, jak się masz?`);
-};
-hello();
-hello("Matys");
+console.log('=== SPREAD ===');
 
-const add = (x = "Podaj liczbę 1 ", y = "Podaj liczbę 2") => console.log(x + y);
-add()
+const arr = [1,2,3,4]
+console.log(arr);
+console.log(...arr);
+
+console.log('');
+console.log('=== REST ===');
+
+const numbers = (x, y, ...z) => {
+	console.log(x, y, z);
+	console.log('');
+	//skoro element z jest tablicą to możemy pracować z nim dokładnie tak samo jak z tablicami
+	console.log('-- każdy element z tablicy "z" * 2 --');
+	console.log(z.map(el => el * 2));
+console.log('');
+	//możemy również wyświetlać nie wszystkie elementy np tylko x, y
+	console.log('-- x + y --');
+	console.log(x + y);
+}
+numbers(11,12,13,14,15, 'string', true)
