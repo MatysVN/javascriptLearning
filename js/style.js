1810074266a8b6c5e66ce9225bbae4f83b011bd8
Mateusz <mateuszskc@gmail.com>
2024-03-31 00:05:45 +0100

    functions - task 2 => Celcius to Fahrenheit converter

--- js/style.js
+++ js/style.js
@@ -1,41 +1,12 @@
-let score;
+console.log(`=== KONWENTER °C na °F ===`);
 
-const add = (x, y) => {
-	score = x + y;
-	console.log(`Wynik dodawania to ${score}`);
+let celcius
+let temp
 
-	// if (score % 2 === 0) {
-	// 	even();
-	// } else {
-	// 	odd();
-	// }
-
-	//zamiast tak długiego if możemy zapisać funkcje warunkową:
-	score % 2 === 0 ? even() : odd();
-};
-
-const even = () => {
-	console.log(`Liczba ${score} jest parzysta.`);
-};
-const odd = () => {
-	console.log(`Liczba ${score} jest nieparzysta.`);
-};
-
-add(10,20)
-
-console.log('');
-console.log('===Zadanie skrócone===');
-//Całe zadanie możemy zapisać dużo krócej
-
-let score2
-
-const add2 = (a,b) => {
-	score2 = a + b;
-	console.log(`Wynik dodawania to ${score2}`);
-
-	score2 % 2 === 0 ? fun1(score2) : fun2(score2)
+const fahrenheit = x => {
+	celcius = x
+	temp = celcius * 1.8 + 32;
+	console.log(`${x}°C = ${temp}°F`);
 }
-const fun1 = (el) => console.log(`Liczba ${el} jest parzysta`);
-const fun2 = (el) => console.log(`Liczba ${el} jest nieparzysta`);
 
-add2(6,3)
+fahrenheit(20)
