05fca0c07be924234449890de95651c66407201e
Mateusz <mateuszskc@gmail.com>
2024-03-30 20:35:02 +0100

    functions - forEach and callback

--- js/style.js
+++ js/style.js
@@ -1,36 +1,27 @@
-//name jest w zakresie GLOBALNYM - czyli dostępna jest z każdego miejsca w naszym kodzie
-const name = 'Marysia'
+const numbers = [0.5, 4, "abc"];
+const names = ['Balbina', 'Marysia', 'Matylda', 'Mateusz']
 
-//ustalając zmienną food bez nadania jej wartości możemy używać jej w każdym zakresie nie musząc tworzyć za każdym razem nowej zmiennej. 
-let food
+numbers.forEach(number => console.log(number * number))
 
-//funcja test również jest w zakresie GLOBALNYM
-const test = () => {
-	//ten const name jest w zakresie LOKALNYM, dlatego możemy użyć nazwy name drugi raz
-	const name = 'Balbinka'
-	//ten console log jest w zakresie LOKALNYM
-	console.log(`NAME w funkcji - ${name}`);
-	//zmienna age jest w zakresie lokalnym dlatego nie możemy wyświetlić jej poza funkcją test
-	const age = 24
-	console.log(age);
+//konsola wyloguje nam undefined ponieważ pętla forEach niczego nam nie zwraca
+const bigNames = names.forEach(name => name.toUpperCase())
+console.log(bigNames);
 
-	const test2 = (name) => {
-		//konsola wyświetli najpiew name z tej funkcji jeżeli nie podamy żadnego name, to przejdzie poziom wyżej do funkcji test i wyświetli zmienną name Balbinka. Jeżeli nadal nie byłoby nadanej zmiennej to przejdzie poziom wyżej i wyświetli dane z zakresu GLOBALNEGO
-		console.log(name);
-	}
-	test2(name)
+//zamiast tego musimy użyć map który zwraca nam nową tablicę
+const realBigNames = names.map(name => name.toUpperCase())
+console.log(realBigNames);
 
-	//funkcja test3 jest funkcją siostrzaną do test2, ale nie ma dostępu do jej wnętrza, tj nie możemy używać tego co przechowuje test2
-	const test3 = () => {
-		const name = "test3 - Matylda"
-		console.log(name);
-	}
-	test3()
-}
-test()
 console.log('');
+console.log('===Callback===');
+//zamiast tego możemy w map dodać callbacka
 
-//ten console.log również jest w zakresie GLOBALNYM
-console.log(`NAME poza funkcją - ${name}`);
+//funkcja służąca  za callback
+const showBigNames = (name) => {
+	console.log(name.toUpperCase());
+}
+const callbackBigNames = names.map(showBigNames)
 
+console.log('===Zapis skrócony===');
 
+//możemy to również zapisać bez callbacka w 1 linijce
+names.forEach(parametr => console.log(parametr.toUpperCase()))
