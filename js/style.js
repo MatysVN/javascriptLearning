93ee31b402744136902d9b4a4c12699dede724bf
Mateusz <mateuszskc@gmail.com>
2024-04-02 00:47:46 +0200

    Creating and adding elements to document

--- js/style.js
+++ js/style.js
@@ -1,7 +1,17 @@
-const heading = document.querySelector('h1')
-const paragraphs = document.querySelectorAll('p')
-const test = document.querySelector('div.test')
-const test2 = test.querySelector('#test')
+const ulList = document.createElement('ul')
+const liItem = document.createElement('li')
 
-console.log(heading, paragraphs, test);
-console.log(test2);
+
+liItem.textContent = 'Cześć' //dodaliśmy do naszego elementu li tekst "Cześć"
+
+document.body.appendChild(ulList) //Musimy sprecyzować gdzie w dokumencie chcemy umieścić element w tym przypadku w body
+ulList.appendChild(liItem) // Dodaj dziecko do naszej ulList którym to dzieckiem niech będzie nasza zmienna liItem w której stworzyliśmy nasze li
+
+//Aby dodać coś precyzyjnie w miejscu które chcemy w kodzie musimy mieć jakiś element w html który posłuży nam za kotwicę do elementu
+//To powinno być poukładane ale dla czytelności daję na dole:
+const testDiv = document.querySelector('.test')
+const paragraph = document.createElement('p')
+
+paragraph.textContent = 'To jest dynamicznie stworzony paragraf'
+
+testDiv.appendChild(paragraph)
