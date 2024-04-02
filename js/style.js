080f66eac4cd214b5a10343c0ccfe44dfedbbcc9
Mateusz <mateuszskc@gmail.com>
2024-04-02 11:30:52 +0200

    innerHTML, textContent, innerText

--- js/style.js
+++ js/style.js
@@ -1,13 +1,35 @@
-const div = document.querySelector('div')
-const p = document.createElement('p')
-const headingTwo = document.createElement('h2')
+const btn = document.querySelector("button");
 
-p.textContent = 'Paragraf dodany w JS'
-headingTwo.textContent = 'h2 dodane w JS'
+console.log(btn.outerHTML); //wypisuje w konsoli <button>Przycisk 🍔 </button>
+console.log(btn.innerHTML); //wypisuje w konsoli Przycisk 🍔
 
-//to jest appenChild - musimy dodawać każdy element osobno, nie jesteśmy w stanie dodać stringu za pomocąa appendChild
-div.appendChild(p)
-div.appendChild(headingTwo)
+// btn.outerHTML = 'Zamień btn na ten tekst' // usunie nam przycisk i zamieni go na tekst
 
-//append jest zapisem nowszym możemy dodać kilka elementów na raz
-div.append(p, headingTwo, 'Cześć')
+// btn.innerHTML = "Zamień tekst w przycisku" // Zamieni nam tekst w przycisku
+// document.body.innerHTML = "" //Wyłączy całą zawartość w body
+
+btn.innerHTML = "<li>to jest element listy </li>"; // doda nam li do html
+btn.textContent = "<li>to jest element listy </li>"; // doda nam cały tekst a nie <li>
+btn.innerText = "<li>to jest element listy </li>"; // doda nam cały tekst a nie <li>
+
+const innerT = document.querySelector('.inner-text') //Zwraca tekst widoczny dla użytkownika, ignorując elementy, które są ukryte przez styl CSS (np. display: none). Usuwa również wszystkie znaczniki HTML i zwraca tylko czysty tekst.
+
+const textC = document.querySelector('.text-content') // Zwraca cały tekst znajdujący się wewnątrz elementu, łącznie z tekstem ukrytym oraz znacznikami HTML. Nie dba o to, czy tekst jest widoczny czy też nie, zwraca wszystkie znaki tekstu wewnątrz elementu, włączając w to także białe znaki i elementy HTML.
+
+console.log(innerT.innerText);
+console.log(textC.textContent);
+
+const input = document.querySelector('input')
+const underInput = document.querySelector('.under-input')
+const underInput2 = document.querySelector('.under-input2')
+
+input.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        underInput.textContent = input.value
+    }
+})
+input.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        underInput2.innerHTML = input.value
+    }
+})
