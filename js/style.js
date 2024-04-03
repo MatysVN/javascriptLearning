bf1ebac1516a53a8111d3a958e5569c874cc6d9f
Mateusz <mateuszskc@gmail.com>
2024-04-03 21:08:48 +0200

    task - 2 addEventListener

--- js/style.js
+++ js/style.js
@@ -1,24 +1,24 @@
-const ulList = document.createElement("ul");
-const number = 10 //stworzone po to aby zmieniac ile razy pętla ma się wykonać
-const allLi = ulList.getElementsByTagName('li') //W zadaniu rozwiązaniem było dać ulList.querySelector('ul:last-child'), ale to nie działa w tym miejscu, trzeba by przesunąć ten kod pod pętlę for gdzie dopiero tam tworzymy wszystkie nasze li
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
+const square = document.querySelector('.gold')
+const text1 = document.querySelector('.text1')
+const text2 = document.querySelector('.text2')
 
-document.body.append(ulList);
-
-for (let i = 1; i <= number; i++) {
-    let liItem = document.createElement('li')
-	liItem.textContent = i;
-	ulList.append(liItem);
+const helloWorld = () => {
+    console.log('Hello World!')
+}
+const redSquare = () => {
+    square.style.backgroundColor = 'red'
+}
+const blueSquare = () => {
+    square.style.backgroundColor = 'royalblue'
+}
+const swapText = () => {
+    text1.classList.toggle('show')
+    text2.classList.toggle('hidden')
 }
 
-allLi[9].textContent = 'Jestem ostatnim elementem.'
-allLi[9].style.backgroundColor = 'royalblue'
-allLi[9].style.padding = '20px 40px'
-allLi[9].style.fontSize = '20px'
-
-/* Zadanie
-1. Stwórz w JS ul listę i dodaj ją na stronę.
-2. Lista ma zawieraać w sobie 10 tagów li, w każdym li ma być wypisana cyfra 1,2,3...
-3. Pobierz ostatni element listy, ale nie dodawaj do niego żadnej klasy / ID!
-4. Zmień zawartość ostatniego elementu na tekst "Jestem ostatnim elementem."
-5. Za pomocą JS, ustal style dla ostatniego elementu
-*/
+btn1.addEventListener('dblclick', helloWorld)
+square.addEventListener('mouseenter', redSquare)
+square.addEventListener('mouseleave', blueSquare)
+btn2.addEventListener('click', swapText)
