99b399cc89f3d881c8883053e71e4ad62b9a85a8
Mateusz <mateuszskc@gmail.com>
2024-04-03 14:38:30 +0200

    styling in JS

--- js/style.js
+++ js/style.js
@@ -1,30 +1,10 @@
-const allCircles = document.querySelectorAll(".circle");
-const lime = document.querySelector(".lime");
-const blue = document.querySelector(".blue");
-const gold = document.querySelector(".gold");
-const newCircle = document.createElement('div') //tworzymy div
+const div = document.querySelector('div')
 
-// allCircles.forEach((circle) =>
-// 	circle.addEventListener("click", () => console.log(circle))
-// );
+div.style.fontSize = '48px'
+div.style.backgroundColor = 'red'
+div.style.padding = '20px 40px'
+div.style.borderRadius = '20px'
 
-newCircle.classList.add('circle','purple')
+// Jednak nie powinniśmy stylować za pomocą JS ponieważ od tego mamy CSS. Zamiast tego możemy pobrać klasę z naszego pliku css
 
-gold.append(newCircle)
-
-lime.addEventListener('click', () => console.log('test')) // teraz nasłuchujemy na cały lime
-
-//ale jeśli chcemy nasłuchiwać tylko na elementy które mają klasę circle2 to:
-lime.addEventListener('click', e => {
-    if(e.target.matches('.circle2')){
-        console.log('test2');
-    }
-})
-//e.target zwraca nam element w który kliknęliśmy .matches('.circle2') szuka czy kliknięty element posiada klasę circle2. Jeżeli to będzie prawda to wtedy odpali nam console.log('test2')
-
-//możemy też odwołać się do classList czyli wszystkich klas naszego elementu (zwróć uwagę że nie ma . przy klasie)
-lime.addEventListener('click', e => {
-    if(e.target.classList.contains('circle2')){
-        console.log('🌞');
-    }
-})
+div.classList.add('divstyle')
