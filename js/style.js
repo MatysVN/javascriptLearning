f8c911ec271a24b8ba5e8e1978732bc16ad0e036
Mateusz <mateuszskc@gmail.com>
2024-04-04 21:35:11 +0200

    math()

--- js/style.js
+++ js/style.js
@@ -1,10 +1,6 @@
-alert('helloWorld') // wyskoczy alert w którym możemy kliknąć ok, można użyć np jeśli hasło które wpisuje user jest za krótkie
+console.log(Math.random()); //generuje nam losową liczbę 0 - 1
 
-if(confirm('Czy na pewno chcesz zamnkąć stronę?')){
-    console.log('ok'); 
-} else {
-    console.log('nie to nie');
-} //podobne do alert, ale mamy pole wyboru OK albo Cancel do których możemy napisać konkretne funkcje. Możemy użyć jeśli użytkownik pisze post na stronie i chce zamknąć przeglądarkę przed dodaniem posta
+console.log(Math.random() * 10); //generuje nam losową liczbę 0 - 10
 
-const name = prompt('Cześć jak masz na imię?', 'Tutaj wpisz swoje imię')
-console.log(name);
+const number = Math.random() * 10; //generuje nam losową liczbę 0 - 10
+console.log(Math.floor(number)); //zaokrągla tą liczbę do liczby całkowitej
