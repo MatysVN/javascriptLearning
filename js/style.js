f6c48186c3ca8fc1ca7f54ca810b4fe77e35a6c5
Mateusz <mateuszskc@gmail.com>
2024-04-04 16:28:29 +0200

    setTime & setInterval

--- js/style.js
+++ js/style.js
@@ -1,24 +1,9 @@
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const square = document.querySelector('.gold')
-const text1 = document.querySelector('.text1')
-const text2 = document.querySelector('.text2')
-
-const helloWorld = () => {
-    console.log('Hello World!')
-}
-const redSquare = () => {
-    square.style.backgroundColor = 'red'
-}
-const blueSquare = () => {
-    square.style.backgroundColor = 'royalblue'
-}
-const swapText = () => {
-    text1.classList.toggle('show')
-    text2.classList.toggle('hidden')
+const test = () => {
+    console.log('🍔');
 }
 
-btn1.addEventListener('dblclick', helloWorld)
-square.addEventListener('mouseenter', redSquare)
-square.addEventListener('mouseleave', blueSquare)
-btn2.addEventListener('click', swapText)
+test() //funkcja pojawi się od razu
+
+setTimeout(test, 2000) //funkcja pojawi się dopiero po 2 sekundach
+
+setInterval(test, 6000) //funkcja wykonuje się co 6 sekund
