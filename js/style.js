7004d9aa8c7f914ea47db5e2494a0d42fb607efb
Mateusz <mateuszskc@gmail.com>
2024-04-05 20:52:20 +0200

    console.table,error,warn

--- js/style.js
+++ js/style.js
@@ -1,15 +1,31 @@
-const allLi = document.querySelectorAll('li')
-let loopNumber = 1
+//każde ID którego użyjemy w HTML automatycznie będzie zmienną w naszym javascript
+console.log(test);
 
+//w konsoli możemy wylogowywać różne rzeczy nie tylko logi
+console.log('Cześć');
+console.log(window.console)
 
+console.warn('To jest ostrzeżenie...')
+console.error('To jest błąd!')
 
-for(const liItem of allLi) {
-    liItem.textContent = loopNumber
-    liItem.dataset.id = loopNumber
-    loopNumber++
-}
+console.table(['Element 1', 123, 'Element 3'])
 
-const middleLi = document.querySelector('[data-id="3"]')
-console.log(middleLi);
+const persons = [
+    {
+        name: 'Matylda',
+        age: 0,
+        favFood: 'Mleko'
+    },
+    {
+        name: 'Mateusz',
+        age: 30,
+        favFood: undefined
+    },
+    {
+        name: 'Marlena',
+        age: 31,
+        favFood: 'Płatki owsiane'
+    },
+]
 
-console.log(middleLi.closest('.wrapper'));
+console.table(persons)
