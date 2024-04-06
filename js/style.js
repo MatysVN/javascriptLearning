7bd805fdd92fb886b9180b4888c235a277467197
Mateusz <mateuszskc@gmail.com>
2024-04-06 21:15:11 +0200

    RegExp and password validator

--- js/style.js
+++ js/style.js
@@ -1,29 +1,44 @@
-const sizeUpBtn = document.querySelector(".sizeUp");
-const sizeDownBtn = document.querySelector(".sizeDown");
-const colorBtn = document.querySelector(".color");
-const text = document.querySelector("p");
-let fontSize = 36;
+const pass = document.querySelector("#password");
+const p = document.querySelector(".passinfo");
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*{}()]/;
+const minValue = 10;
 
-const sizeDown = () => {
-	if (fontSize <= 20) return;
-	 //jeżeli font size dojdzie do 20px to return kończy wykonywanie funkcji
-
-	fontSize -= 2;
-	text.style.fontSize = fontSize + "px";
+//tworzymy zmienną która wyświetla czy nasze hasło spełnia warunki
+const showMsg = () => {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		//jeśli warunki zostaną spełnione to w paragrafie wypisz tekst
+		p.textContent = "Masz bardzo dobre hasło!";
+		p.style.color = "lime";
+	} else if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers)
+		) {
+			p.textContent = "Masz dobre hasło!"
+			p.style.color = 'gold'
+		} else {
+			p.textContent = "Masz za słabe hasło!"
+			p.style.color = 'tomato'
+			
+		}
 };
-const sizeUp = () => {
-	fontSize += 2;
-	text.style.fontSize = fontSize + "px";
-};
 
-const changeColor = () => {
-	let r = Math.floor(Math.random() * 255);
-	let g = Math.floor(Math.random() * 255);
-	let b = Math.floor(Math.random() * 255);
 
-	text.style.color = `rgb(${r},${g},${b})`;
-};
 
-sizeDownBtn.addEventListener("click", sizeDown);
-sizeUpBtn.addEventListener("click", sizeUp);
-colorBtn.addEventListener("click", changeColor);
+const checkPassword = () => {
+	if(pass.value.length !== 0) {
+		showMsg()
+	} else {
+		p.textContent = 'Nie podałeś hasła...'
+		p.style.color = ''
+	}
+}
+
+pass.addEventListener('keyup', checkPassword)
