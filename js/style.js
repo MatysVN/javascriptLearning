b2970cde92e302cfc680c88961d7e9ee357b750c
Mateusz <mateuszskc@gmail.com>
2024-04-07 23:26:25 +0200

    project 4 - Celcius to Fahrenheit converter - create doc

--- js/style.js
+++ js/style.js
@@ -1,44 +1,20 @@
-const pass = document.querySelector("#password");
-const p = document.querySelector(".passinfo");
-const letters = /[a-z]/i;
-const numbers = /[0-9]/;
-const special = /[!@#$%^&*{}()]/;
-const minValue = 10;
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const changeBtn = document.querySelector('.change')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
 
-//tworzymy zmienną która wyświetla czy nasze hasło spełnia warunki
-const showMsg = () => {
-	if (
-		pass.value.length > minValue &&
-		pass.value.match(letters) &&
-		pass.value.match(numbers) &&
-		pass.value.match(special)
-	) {
-		//jeśli warunki zostaną spełnione to w paragrafie wypisz tekst
-		p.textContent = "Masz bardzo dobre hasło!";
-		p.style.color = "lime";
-	} else if (
-		pass.value.length > minValue &&
-		pass.value.match(letters) &&
-		pass.value.match(numbers)
-		) {
-			p.textContent = "Masz dobre hasło!"
-			p.style.color = 'gold'
-		} else {
-			p.textContent = "Masz za słabe hasło!"
-			p.style.color = 'tomato'
-			
-		}
-};
+let fahrenheit
+let celcius
 
+// °F = (°C × 1.8) + 32
+// °C = (°F − 32) /1.8
 
-
-const checkPassword = () => {
-	if(pass.value.length !== 0) {
-		showMsg()
-	} else {
-		p.textContent = 'Nie podałeś hasła...'
-		p.style.color = ''
-	}
+const change = () => {
+	one.textContent = "°F"
+	two.textContent = "°C"
 }
 
-pass.addEventListener('keyup', checkPassword)
+changeBtn.addEventListener('click', change)
