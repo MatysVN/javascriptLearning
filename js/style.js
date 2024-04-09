9402caed8e5b92d87bbf11e921e70b29483418fe
Mateusz <mateuszskc@gmail.com>
2024-04-09 22:15:37 +0200

    burger menu

--- js/style.js
+++ js/style.js
@@ -1,66 +1,24 @@
-const converter = document.querySelector("#converter");
-const result = document.querySelector(".result");
-const convBtn = document.querySelector(".conv");
-const resetBtn = document.querySelector(".reset");
-const changeBtn = document.querySelector(".change");
-let one = document.querySelector(".one");
-let two = document.querySelector(".two");
+const burger = document.querySelector('.burger')
+const burgerBtn = document.querySelector('.fa-bars')
+const crossBtn = document.querySelector('.fa-times')
+const ulList = document.querySelector('nav ul')
+const allLi = ulList.querySelectorAll('li')
 
-let fahrenheit;
-let celcius;
-
-// °F = (°C × 1.8) + 32
-// °C = (°F − 32) /1.8
-
-const swapDeegres = () => {
-	if (one.textContent === "°C") {
-		one.textContent = "°F";
-		two.textContent = "°C";
-		result.textContent = ""
-	} else {
-		one.textContent = "°C";
-		two.textContent = "°F";
-		result.textContent = ""
-	}
-};
-
-const deegres = () => {
-	if (one.textContent === "°C") {
-		const cTF = () => {
-			fahrenheit = converter.value * 1.8 + 32;
-			result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
-		};
-		cTF();
-	
-	} else {
-		const ftC = () => {
-			celcius = (converter.value - 32) / 1.8;
-			result.textContent = `${converter.value}°F to ${celcius.toFixed(1)}°C`;
-		};
-		ftC();
-
-	}
+const showNav = () => {
+	ulList.classList.toggle('active'); // przesuwa nam element na osi X o 350px
+	burgerBtn.classList.toggle('hide') // nadaje display none na element
+	crossBtn.classList.toggle('hide')
+}
+const closeNav = () => {
+	ulList.classList.remove('active')
 }
 
-const conversion = () => {
-	//jeżeli wpisana wartość jest różna od pustego stringu wykonaj funkcje deegres, jeśli nie to wypisz wiadomość
-	if(converter.value !== "") {
-deegres()
-	} else {
-		result.textContent = "Musisz podać jakąś wartość!"
-	}
-}
+//trzeba nadać forEach na allLi tak żeby funkcja była aktywna na każdym elemencie li. Zamiast nazwy "li" możemy użyć dowolnej
+allLi.forEach(li => li.addEventListener('click', closeNav))
 
-const reset = () => {
-	result.textContent = ""
-	converter.value = ""
-	one.textContent = "°C"
-	two.textContent = "°F"
-}
+burger.addEventListener('click', showNav);
+console.log(allLi);
 
-convBtn.addEventListener("click", conversion);
-changeBtn.addEventListener("click", swapDeegres);
-resetBtn.addEventListener('click', reset)
-
-//.toFixed(1) ustala nam że mamy miec maksymalnie 1 liczbę po przecinku
-//converter.value = "" usuwa nam wpisaną liczbę po wciśnięciu przycisku konwertuj
+/* 1.Po kliknięciu przycisku burgerBtn dodaje klase .active do ulList oraz nadaje na burgerBtn klasę .hide, a z crossBtn ją usuwa
+2.Po kliknięciu w crossBtn usuwa klase .actie z ulList oraz nadaje na crossBtn klasę .hide, a na burgerBtn usuwa tą klase
+3.Po kliknięciu w liItem zamyka nawigacje */
